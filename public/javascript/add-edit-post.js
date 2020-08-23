6b011d10d310776cbc1195e59b05b0154efd35d9
ElK <62822011+evkonradi@users.noreply.github.com>
2020-08-22 21:20:09 -0500

    add and edit post form is added

---
+++ public/javascript/add-edit-post.js
@@ -0,0 +1,54 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_content = document.querySelector('input[name="post-content"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        post_content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+
+  async function editFormHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const post_content = document.querySelector('input[name="post-content"]').value;
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+    
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title, post_content
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+  
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
