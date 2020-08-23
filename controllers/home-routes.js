f7e8c51d17eb4f565d69221a5c8aa7489eae2bc9
ElK <62822011+evkonradi@users.noreply.github.com>
2020-08-22 20:32:16 -0500

    dashboard form is added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,14 +31,24 @@ router.get('/', (req, res) => {
     });
 });
 
-/* router.get('/login', (req, res) => {
+router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
     return;
   }
 
   res.render('login');
-}); */
+}); 
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+}); 
+
 
 /* router.get('/post/:id', (req, res) => {
   Post.findOne({
