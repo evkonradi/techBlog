890e572ad435f30f8930a36010fa95ca84b18b57
ElK <62822011+evkonradi@users.noreply.github.com>
2020-08-23 21:55:44 -0500

    CSS styles are added

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -32,7 +32,7 @@ router.get('/', (req, res) => {
         .then(dbPostData => {
           // serialize data before passing to template
           const posts = dbPostData.map(post => post.get({ plain: true }));
-          res.render('dashboard', { posts });
+          res.render('dashboard', { posts, loggedIn: true });
         })
         .catch(err => {
           console.log(err);
@@ -64,7 +64,7 @@ router.get('/post-edit/:id', (req, res) => {
   .then(dbPostData => {
     // serialize data before passing to template
     const post = dbPostData.get({ plain: true });
-    res.render('post-edit', { post });
+    res.render('post-edit', { post, loggedIn: true });
   })
   .catch(err => {
     console.log(err);
@@ -73,7 +73,7 @@ router.get('/post-edit/:id', (req, res) => {
 });
 
 router.get('/post-add', (req, res) => {
-  res.render('post-add');
+  res.render('post-add', {loggedIn: true});
 });
 
 
