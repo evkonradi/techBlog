736390c2538bd45fa16fb0c1341d5ce20ef17632
ElK <62822011+evkonradi@users.noreply.github.com>
2020-08-23 22:41:08 -0500

    signup form fixed

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -21,4 +21,4 @@ async function signupFormHandler(event) {
     }
 }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', signupFormHandler);
